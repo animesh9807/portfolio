a4fd9eae7ed1222317b4745582b2fc0f1507693a
animesh9807 <84018266+animesh9807@users.noreply.github.com>
2021-08-17 21:20:18 +0530

    mail.php

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@ $subject = "Mail from your website ";
 
 
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject = " . $user_subject . "\r\n Message =" . $message;
-$headers = "From: animesh9807.github.io/portfolio" . "\r\n" .
+$headers = "From: animesh9807@gmail.com" . "\r\n" .
 "CC: ";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
